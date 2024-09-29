0175af4f7a392eaba06abefda8598350deca92ed
Dhiya Ulhaq Ramadhan <162105398+Dhiyaulll@users.noreply.github.com>
2024-09-29 07:36:15 +0700

    TP Modul 1

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/TP.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //soal input output
+    /*
+    string nama, nim;
+    cout << "Siapa nama anda? ";
+    cin >> nama;
+    cout << "Berapa nim anda? ";
+    cin >> nim;
+    cout << "Nama saya:" << nama << endl;
+    cout << "NIM saya:" << nim << endl;
+    return 0;
+    */
+
+    //operasi aritmatika
+    /*
+    int bil1 = 3, bil2 = 4, hasil1;
+    float bil3 = 3.0, bil4 = 4.0, hasil2;
+    hasil1 = bil1 + bil2;
+    cout << hasil1 << endl;
+    hasil1 = bil1 - bil2;
+    cout << hasil1 << endl;
+    hasil1 = bil1 * bil2;
+    cout << hasil1 << endl;
+    hasil1 = bil1 / bil2;  // integer division
+    cout << hasil1 << endl;
+    hasil1 = bil2 / bil1;  // integer division
+    cout << hasil1 << endl;
+    hasil1 = bil1 % bil2;  // modulo
+    cout << hasil1 << endl;
+    hasil1 = bil2 % bil1;  // modulo
+    cout << hasil1 << endl;
+    hasil2 = bil3 / bil4;
+    cout << hasil2 << endl;
+    return 0;
+    */
+
+    //operasi perbandingan
+    /*
+    int bil1 = 2, bil2 = 3, hasil;
+    hasil = bil1 > bil2;
+    cout << hasil << endl;
+    hasil = bil1 >= bil2;
+    cout << hasil << endl;
+    hasil = bil1 < bil2;
+    cout << hasil << endl;
+    hasil = bil1 <= bil2;
+    cout << hasil << endl;
+    hasil = bil1 == bil2;
+    cout << hasil << endl;
+    hasil = bil1 != bil2;
+    cout << hasil << endl;
+    return 0;
+    */
+
+
+    //operasi logika
+    /*
+    int bil1 = 2, bil2 = 3, hasil;
+    hasil = bil1 <= bil2 and bil1 < bil2;
+    cout << hasil << endl;
+    hasil = bil1 >= bil2 or bil1 < bil2;
+    cout << hasil << endl;
+    hasil = not(bil1 >= bil2) or bil1 < bil2;
+    cout << hasil << endl;
+    return 0;
+    */
+
+    //percabangan if-else
+    /*
+    int nilai;
+    cin >> nilai;
+    if (nilai > 80) {
+        cout << "A" << endl;
+    } else {
+        cout << "Bukan A" << endl;
+    }
+    return 0;
+    */
+
+    //perulangan for-to-do
+    /*
+    int bilangan, asli, jumlah;
+    cout << "Masukkan bilangan asli: ";
+    cin >> asli;
+
+    bilangan = 1;
+    jumlah = 0;
+
+    while (bilangan <= asli) {
+        if (bilangan % 2 == 0) {
+            jumlah += bilangan;
+        }
+        bilangan++;
+    }
+    cout << "Jumlah bilangan genap: " << jumlah << endl;
+    return 0;
+    */
+
+
+}
+
