9a6fb8cbaa5eb10acba4ba552a488b070c187ed0
Dhiya Ulhaq Ramadhan <162105398+Dhiyaulll@users.noreply.github.com>
2024-09-29 07:36:54 +0700

    Source Code Guided

---
+++ 01_Pengenalan_CPP_Bagian_1/Guided/guided.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+
+{
+    //Tipe Data
+    /*
+    int angka = 20;
+    float desimal = 0.5;
+    string kalimat = "Dhyul";
+    double tinggi = 10.5;
+    char jenis_kelamin = 'pria';
+    bool isSunny = true;
+
+    cout <<"Angka: " << angka << endl;
+    */
+
+    /*
+    int angka;
+
+    cout << "Masukan angka: ";
+    cin >> angka;
+
+    cout << "Angka: " << angka << endl;
+
+    getch();
+    */
+
+    //operator aritmatika
+    /*
+    int angka1 = 10;
+    int angka2 = 2;
+
+    int hasil = angka1 % angka2;
+
+    cout <<"Hasilnya adalah " << hasil << endl;
+    */
+
+    //operator perbandingan
+    /*
+    int angka1 = 3;
+    int angka2 = 5;
+
+    bool hasil = (angka1 == angka2);
+
+    cout <<"Hasilnya adalah " <<boolalpha <<hasil <<endl;
+    */
+
+    //Operator logika
+    /*
+    bool kondisi1 = true;
+    bool kondisi2 = true;
+
+    bool hasil = (kondisi1 || kondisi2 );
+    cout << "Hasilnya adalah " <<boolalpha <<hasil <<endl;
+    */
+    /*
+    bool kondisi3 = false;
+    bool hasil = !kondisi3;
+    cout << "Hasilnya adalah " <<boolalpha <<hasil <<endl;
+    */
+
+    //perbandingan
+    /*
+    string kata;
+    cout <<"Masukan kata = Halo " <<endl;
+    cin >> kata;
+
+    if(kata == "Halo"){
+        cout << "Kata sesuai" <<endl;
+
+        } else {
+        cout << "Kata tidak sesuai" <<endl;
+    }
+    */
+
+    //Kondisional switch
+    /*
+    int tv;
+    cout << "Daftar channel TV" << endl;
+    cout << "1. Global TV" << endl;
+    cout << "2. RCTI" << endl;
+    cout << "Masukkan channel pilihan: ";
+    cin >> tv;
+
+    switch(tv) {
+        case 1:
+            cout << "Channel yang anda pilih Global TV" << endl;
+            break;
+        case 2:
+            cout << "Channel yang anda pilih RCTI" << endl;
+            break;
+        default:
+            cout << "Channel tidak tersedia" << endl;
+    }
+
+    return 0;
+    */
+    //Perulangan
+    /*
+    for(int i=0; i<5; i++){
+        cout<<i+1 << " Hello world " <<endl;
+    }
+    */
+}
